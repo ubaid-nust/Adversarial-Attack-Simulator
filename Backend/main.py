5258bda3b6ddf1632044dee5c8811711dec93d2b
Khawaja Ubaid <ubaidmohammad901@gmail.com>
2025-12-08 01:06:55 +0500

    newOne

--- Backend/main.py
+++ Backend/main.py
@@ -1,17 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.app_fgsm import app as fastapi_app
 
-# Create FastAPI application
 app = FastAPI()
 
-# Mount your existing app inside this main app
-app.mount("/", fastapi_app)
+# CORS for your frontend
+origins = ["https://main.d379q9b3vj57kv.amplifyapp.com"]
 
-# CORS for your Amplify frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://main.d379q9b3vj57kv.amplifyapp.com"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
