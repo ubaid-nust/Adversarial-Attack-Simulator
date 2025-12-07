4c860ea9073844bdf024f2a7ceecc20c49a90bda
Khawaja Ubaid <ubaidmohammad901@gmail.com>
2025-12-07 19:59:20 +0500

    Updated Build Options

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-};
+  output: 'export',     // <-- enables static export
+  trailingSlash: true,  // optional, ensures URLs end with '/'
+}
 
 export default nextConfig;
